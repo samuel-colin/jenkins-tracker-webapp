e509671a895711f911b02cad992bde1368456704
samuel.colin <samuel.colin@live.fr>
2016-09-20 08:35:04 +0200

    New version =)

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
-require_once("sources/functions_ui.php");
 
-$GLOB_url_jenkins = "http://localhost:8080";
-
-//Header
-echo display_header();
-
-//Content
-echo display_content("Liste des jobs DEV", "ressources/jobs_dev.properties", $GLOB_url_jenkins);
-
-//Footer
-echo display_footer();
+    require_once("engine/functions_ui.php");
+    
+    echo displayHeader();
+    echo display("ressources/views.properties");
+    echo displayFooter();
 
 ?>
